219ba38e6d04cab01ff2551c8237e6e3bdf8933f
c9dong <davidCdong_123@hotmail.com>
2017-07-09 20:56:27 -0400

    first ga

--- GA.py
+++ GA.py
@@ -1,65 +1,150 @@
 import random
 import sys
+import math
+
+from graph import graph
+
+'''
+5 bits per gene:
+    b0b1b2: directional movement
+    b3: move up in z if 1
+    b4: move down in zif 1
+    if b3 == 0 and b4 == 0: stay on same level
+    if b3 == 1 and b4 == 1: invalid gene, remove
+
+generate N * M genes per chromosome (size of map)
+'''
 
 geneSet = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!.,"
 target = "Hi, my name is Jake Si! I like apples."
 
+test_map = graph.Graph()
 
 class Chromosome:
 
-    def __init__(self, gene, generation=0):
+    def __init__(self, gene, length, generation=0):
         self.gene = gene
-        self.fitness = Chromosome.update_fitness(gene)
+        self.fitness = Chromosome.update_fitness(gene, length)
         self.generation = generation
+        self.length = length;
+
+    def getGeneAtIndex(self, index):
+        return int(self.gene[2*index] + self.gene[2*index+1])
+
+    def replaceGeneAtIndex(self, index, n):
+        strN = str(n)
+        if n < 10:
+            strN = '0' + strN
+        newGene = self.gene[:2*index] + strN + self.gene[2*index+2:]
+        self.gene = newGene
+        assert(len(self.gene)%2 == 0)
+
+    def exchange(self, index1, index2, mate):
+        # index1 <= index2
+        left = self.gene[:index1]
+        right = self.gene[index2:]
+        middle = mate.gene[index1:index2]
+        self.gene = left + middle + right
+        assert(len(self.gene)%2 == 0)
 
     def crossover(self, mate):
         '''
         Two point crossover
         '''
         generation = max(self.generation, mate.generation)
-        index1 = random.randint(1, len(target) - 2)
-        index2 = random.randint(1, len(target) - 2)
+        index1 = random.randint(1, self.length - 2)
+        index2 = random.randint(1, self.length - 2)
         if index1 > index2:
             index1, index2 = index2, index1
-        child1 = self.gene[:index1] + \
-            mate.gene[index1:index2] + self.gene[index2:]
-        child2 = mate.gene[:index1] + \
-            self.gene[index1:index2] + mate.gene[index2:]
-        # print parent1, parent2 ,child1, index
-        return Chromosome(child1, generation + 1), Chromosome(child2, generation + 1)
+
+        self.exchange(index1, index2, mate)
+        mate.exchange(index1, index2, self)
+        return Chromosome(self.gene, self.length, self.generation+1), \
+            Chromosome(mate.gene, mate.length, mate.generation+1)
 
     def mutate(self):
         '''
         Random index mutation
         '''
-        index = random.randrange(0, len(self.gene))
-        childGenes = list(self.gene)
-        newGene, alternate = random.sample(geneSet, 2)
-        childGenes[index] = alternate if newGene == childGenes[
-            index] else newGene
-        return Chromosome(''.join(childGenes), self.generation + 1)
+        index = random.randrange(0, self.length)
+        oldGene = self.getGeneAtIndex(index)
+        newGene = oldGene
+        while newGene == oldGene:
+            newGene = random.getrandbits(5)
+        self.replaceGeneAtIndex(index, newGene)
+        return Chromosome(self.gene, self.length, self.generation+1)
 
     @staticmethod
-    def update_fitness(gene):
-        '''
-        The difference between each letter's ASCII value from the target
-        '''
-        sum = 0
-        for expected, actual in zip(target, gene):
-            sum += abs(ord(actual) - ord(expected))
-        return sum
+    def getMovements(chromo, length):
+        movements = []
+        tempChromo = chromo
+        for i in range(length):
+            gene = int(chromo[2*i] + chromo[2*i+1])
+            movement = gene & 7
+            gene = gene >> 3
+            moveUp = gene & 1
+            gene = gene >> 1
+            moveDown = gene & 1
+
+            if moveUp == 1 and moveDown == 1:
+                # do not move
+                continue
+
+            moveX = 0
+            moveY = 0
+            moveZ = 0
+
+            if moveUp == 1:
+                moveZ = 1
+            if moveDown == 1:
+                moveZ = -1
+            if movement == 0:
+                moveX = -1
+            elif movement == 1:
+                moveX = 1
+            elif movement == 2:
+                moveY = -1
+            elif movement == 3:
+                moveY = 1
+            elif movement == 4:
+                moveX = -1
+                moveY = -1
+            elif movement == 5:
+                moveX = -1
+                moveY = 1
+            elif movement == 6:
+                moveX = 1
+                moveY = -1
+            elif movement == 7:
+                moveX = 1
+                moveY = 1
+            else:
+                assert(False)
+            movements.append((moveX, moveY, moveZ))
+        return movements
 
     @staticmethod
-    def gen_random():
-        genes = []
-        while len(genes) < len(target):
-            sampleSize = min(len(target) - len(genes), len(geneSet))
-            genes.extend(random.sample(geneSet, sampleSize))
-        return Chromosome(''.join(genes))
+    def update_fitness(gene, length):
+        movements = Chromosome.getMovements(gene, length)
+        return test_map.calcMovementPenalty(movements)
+
+    @staticmethod
+    def gen_random(length):
+        '''
+        Generate random 5 bit genes
+        '''
+        chromo = ''
+        for i in range(length):
+            gene = random.getrandbits(5)
+            if gene < 10:
+                chromo = chromo + '0'
+            chromo = chromo + str(gene)
+        assert(len(chromo)%2 == 0)
+        return Chromosome(chromo, length)
 
 class Population:
 
-    def __init__(self, tournamentSize=3, mutationRate=0.1, crossoverRate=0.9, elitismRate=0.1, populationSize=2000):
+    def __init__(self, tournamentSize=3, mutationRate=0.1, crossoverRate=0.9, elitismRate=0.1, populationSize=500):
         self.population = []
         self.tournamentSize = tournamentSize
         self.mutationRate = mutationRate
@@ -68,12 +153,13 @@ class Population:
         self.populationSize = populationSize
         self.bestParent = None
 
-    def display(self, guess):
-        print("{0}\t\t{1}\t{2}".format(guess.gene, guess.fitness, guess.generation))
+    # def display(self, guess):
+    #     print("{0}\t\t{1}\t{2}".format(guess.gene, guess.fitness, guess.generation))
 
     def generate_population(self):
         for i in range(0, self.populationSize):
-            self.population.append(Chromosome.gen_random())
+            chromo = Chromosome.gen_random(test_map.width * test_map.height)
+            self.population.append(chromo)
 
 
     def tournament_selection(self):
@@ -88,11 +174,16 @@ class Population:
     def setup(self):
         self.generate_population()
         self.population = sorted(self.population, key=lambda x: x.fitness)
+        for p in self.population:
+            print p.fitness
         self.bestParent = self.population[0]
-        self.display(self.bestParent)
+        print self.bestParent.fitness
 
     def evolve(self):
-        while True:
+        count = 0
+        while count < 100:
+            count += 1
+            print count
             size = self.populationSize
             idx = int(round(size * self.elitismRate))
             buf = self.population[:idx]
@@ -113,10 +204,9 @@ class Population:
             bestChild = self.population[0]
             if self.bestParent.fitness <= bestChild.fitness:
                 continue
-            self.display(bestChild)
-            if bestChild.fitness <= 0:
-                return
             self.bestParent = bestChild
+            print self.bestParent.fitness
+        test_map.plotMovement(Chromosome.getMovements(self.bestParent.gene, self.bestParent.length))
 
 def main():
     random.seed()
--- graph/graph.py
+++ graph/graph.py
@@ -4,7 +4,7 @@ import numpy as np
 INFINITY = 99999999
 
 class Graph(object):
-  def __init__(self, mapfile__name='waterloo.map', start_pos=(0,0,0)):
+  def __init__(self, mapfile__name='graph/waterloo.map', start_pos=(0,0,0)):
     self.map = []
     with open(mapfile__name, 'rb') as mapfile:
       while True:
@@ -13,18 +13,35 @@ class Graph(object):
           break;
         row = map(float, row.split(' '))
         self.map.append(row)
-    self.height = len(self.map)
-    self.width = len(self.map[0])
+    self.height = len(self.map) / 5
+    self.width = len(self.map[0]) / 5
     self.start_pos = (start_pos[0], start_pos[1], self.map[0][0]+1)
+    self.finish_pos = (self.width - 1, self.height - 1, self.get(self.width-1, self.height-1)+1)
     self.pos = (start_pos[0], start_pos[1], self.map[0][0]+1)
     self.movement = [self.pos]
 
-  def get(x, y):
+  def get(self, x, y):
     return self.map[y][x]
 
-  def setStart(pos):
+  def setStart(self, pos):
     self.start_pos = (pos[0], pos[1], self.map[0][0]+1)
     self.pos = (start_pos[0], start_pos[1], self.map[0][0]+1)
+
+  def calcMovementPenalty(self, movements):
+    lastPos = self.start_pos;
+    curPos = (0, 0, 0)
+    penalty = 0
+    for movement in movements:
+      # add 1 penalty for each movement
+      penalty += 1
+      curPos = (lastPos[0] + movement[0], lastPos[1] + movement[1], lastPos[2] + movement[2])
+      curPenalty = self.calcPenalty(lastPos, curPos)
+      # if (curPenalty == INFINITY):
+      #   # return immidiately if it's an invalid move
+      #   return INFINITY
+      penalty += curPenalty
+      lastPos = curPos
+    return penalty
 
   # x, y, z E [-1, 1]
   def move(self, x, y, z):
@@ -36,11 +53,14 @@ class Graph(object):
 
   def calcPenalty(self, lastPos, pos):
     if pos[0] < 0 or pos[0] >= self.width:
-      return INFINITY
+      return 100
     if pos[1] < 0 or pos[1] >= self.height:
-      return INFINITY
+      return 100
     if pos[2] <= self.get(pos[0], pos[1]):
-      return INFINITY
+      return 100
+    if pos == self.finish_pos:
+      return -1000
+    return 0
 
   def plot(self):
     coord = np.array(self.map, dtype=float)
@@ -54,7 +74,11 @@ class Graph(object):
       scatX.append(scatX[len(scatX)-1]+m[0])
       scatY.append(scatY[len(scatY)-1]+m[1])
 
-    coord = np.array(self.map, dtype=float)
+    new_map = []
+    for i in range(self.height):
+      new_map.append(self.map[i][:self.width])
+
+    coord = np.array(new_map, dtype=float)
     plt.imshow(coord, cmap='hot', interpolation='nearest')
     plt.scatter(scatX, scatY, s=3)
     plt.show()
