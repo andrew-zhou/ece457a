856b754158de7d5707fa15a73292396964c10dce
c9dong <davidCdong_123@hotmail.com>
2017-07-24 20:48:40 -0400

    pdf

--- ga/main.py
+++ ga/main.py
@@ -8,10 +8,25 @@ from iomanager import IOManager
 from iomanager import GraphNode
 
 from population import Population
+from chromosome import Chromosome
 
 from graph import Graph
 
 from gaconstants import START_NODE
+
+def remove_cycle(path):
+  new_path = copy.deepcopy(path)
+  for i in range(len(new_path)):
+    if i >= len(new_path): break
+    n1 = new_path[i]
+    for j in range(i+1, len(new_path)):
+      if j >= len(new_path): break
+      n2 = new_path[j]
+      if n1 == n2:
+        new_path = new_path[:i] + new_path[j:]
+        i-=1
+        break
+  return new_path
 
 def main():
   parser = argparse.ArgumentParser()
@@ -25,12 +40,16 @@ def main():
   # graph = IOManager.import_graph('../aco/waterloo.ecegraph')
 
   paths = []
+  costs = []
   removed = {}
   start_time = time.time()
   for goal in goals:
     end = goal
     population = Population(graph[START_NODE], end, removed)
     path = population.evolve()
+    path = remove_cycle(path)
+    new_cost = Chromosome.calc_costs(path, graph, goal, removed)
+    costs.append(new_cost)
     paths.append(path)
     # remove path from graph
     for p in path:
@@ -42,6 +61,8 @@ def main():
     #     del graph[s].neighbours[e]
     #   s = e
   end_time = time.time()
+  print 'path: ' + str(paths)
+  print 'costs: ' + str(costs)
   print('Overall time: {}'.format(end_time - start_time))
 
   # if graph != None:
